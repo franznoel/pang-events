c7039031d7473d05f09ded2c6bf74efbf89e0157
Franz Noel Tanglao <franz.tanglao@franzs-mbp.attlocal.net>
2016-01-09 00:44:08 -0800

    v1.0.5 Improved validation, refactored functions, and created user-profile page.

--- app/script.js
+++ app/script.js
@@ -36,5 +36,78 @@ function createValidation(methodName,regex,message) {
   );
 }
 
+function preparePasswordValidation() {
+  createValidation("passwordHasNumber",/\d/,"Passwords should have a number.");
+  createValidation("passwordHasUppercase",/[A-Z]/,"Passwords should have uppercase.");
+  createValidation("passwordHasSpecialCharacters",/[^a-zA-Z\d]/,"Passwords should have special characters such as exclamation points (!), question marks (?), hashtags (#), periods(.), commas(,), or slash(/).");
+}
+
+function createCompleteEmailValidation() {
+  var completeEmailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  createValidation("completeEmail",completeEmailRegex,"Passwords should have special characters such as exclamation points (!), question marks (?), hashtags (#), periods(.), commas(,), or slash(/).");
+}
+
+function createErrorMessage(type,message,errorList,code) {
+  var html= "";
+  html+= "<div class='error ";
+  html+= (type=="success") ? "success" : "alert";
+  html+= " errorContainer callout'>";
+  html+= (type=="success") ? "<h5><i class='fi-checkbox'></i> Cool!</h5>" : "<h5><i class='fi-alert'></i> Oops!</h5>";
+  html+= "<p>"+ message +"</p>"
+  html+= "</div>";
+  return html;
+}
+
+function displayErrorMessage(html) {
+  $("#errorMessage").html(html);
+}
+
+function resetPassword(ref,emailAddress) {
+  ref.resetPassword({
+    email: emailAddress
+  },function(error) {
+    var errorMessage = "";
+    if (error) {
+      switch (error.code) {
+        case "INVALID_USER":
+          errorMessage = createErrorMessage("error","The specified user account does not exist.");
+          break;
+        default:
+          errorMessage = createErrorMessage("error","Error resetting password:" + error + ".");
+      }
+    } else {
+      var message = "Password reset and email sent successfully! ";
+      message+= "Check email for the password and <a href='/sign-in.html'>sign in</a>.";
+      errorMessage = createErrorMessage("success",message);
+    }
+    displayErrorMessage(errorMessage);
+  });
+}
+
+function changePassword(ref,emailAddress,oldPassword,newPassword) {
+  ref.changePassword({
+    email: emailAddress,
+    oldPassword: oldPassword,
+    newPassword: newPassword
+  }, function(error) {
+    var errorMessage = "";
+    if (error) {
+      switch (error.code) {
+        case "INVALID_PASSWORD":
+          errorMessage = createErrorMessage("error","The specified user account password is incorrect.")
+          break;
+        case "INVALID_USER":
+          errorMessage = createErrorMessage("error","The specified user account does not exist.")
+          break;
+        default:
+          errorMessage = createErrorMessage("error","Error changing password:"+error)
+      }
+    } else {
+      errorMessage = createErrorMessage("success","User password changed successfully!");
+    }
+    displayErrorMessage(errorMessage);
+  });
+}
+
 
 $(document).foundation();
