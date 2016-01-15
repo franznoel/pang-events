5cc245b7de258e700393340733823ed1527546fc
Franz Noel Tanglao <franz.tanglao@franzs-mbp.attlocal.net>
2016-01-14 17:13:39 -0800

    v1.0.15 Minified styles using gulp.

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -2,31 +2,49 @@ var gulp = require('gulp'),
   clean = require('gulp-clean'),
   gpconcat = require('gulp-concat'),
   gprename = require('gulp-rename'),
+  cssnano = require('gulp-cssnano'),
   uglify = require('gulp-uglify');
 
-
-gulp.task('clean-scripts',function() {
-  return gulp.src("app.js",{read : false})
-    .pipe(clean());
-});
-
-gulp.task('scripts', ['clean-scripts'], function () {
-  var sources = [
+var PATH = {
+  scripts: [
     'bower_components/jquery/dist/jquery.js',
     'bower_components/jquery-validation/dist/jquery.validate.js',
     'bower_components/jquery-validation/dist/additional-methods.js',
-    './bower_components/foundation-sites/dist/foundation.js',
+    'bower_components/foundation-sites/dist/foundation.js',
     'script.js'
-  ];
+  ],
+  css: [
+    'bower_components/foundation-sites/dist/foundation.css',
+    'bower_components/foundation-icon-fonts/foundation-icons.css',
+    'style.css'
+  ]
+}
 
-  gulp.src(sources) //,{read:false}
+gulp.task('clean-scripts',function() {
+  return gulp.src(["scripts/app.js","css/style.css"],
+    {read : false})
+    .pipe(clean());
+});
+
+// concatenate and uglify all CSS of the page, except internal
+gulp.task('styles', ['clean-scripts'], function () {
+  gulp.src(PATH.css)
+    .pipe(cssnano())
+    .pipe(gpconcat('style.css'))
+    .pipe(gulp.dest('css'));
+});
+
+// concatenate and uglify all JavaScript of the page, except internal
+gulp.task('scripts', ['clean-scripts'], function () {
+  gulp.src(PATH.scripts)
     .pipe(gpconcat('app.js'))
     .pipe(gulp.dest('scripts'))
     .pipe(uglify())
     .pipe(gulp.dest('scripts'));
 });
 
-gulp.task('default',['scripts']);
+// gulp.task('default',['styles','scripts']);
+gulp.task('default',['styles','scripts']);
 
 
 
